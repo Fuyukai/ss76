62f24eb3170356715341dcc473e3c5125d9ae7c7
Lura Skye Revuwution <l@veriny.tf>
2021-08-24 00:15:42 +0100

    fix date scene

--- ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/vn/sussex/July4Sussex.kt
+++ ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/vn/sussex/July4Sussex.kt
@@ -7,7 +7,7 @@ import tf.veriny.ss76.scene.text.createAndRegisterScene
 private const val JULY_4_FORECAST = "It's a sunny day. It's even hotter than yesterday. " +
                                     "Small clouds break up the skyline."
 private val JULY_4_DATE_SCENE = buildDateMarkerScene(
-    "sussex-july-4-start", "Monday", 3, JULY_4_FORECAST,
+    "sussex-july-4-start", "Tuesday", 4, JULY_4_FORECAST,
     "sussex-july-4-school-1"
 )
 
