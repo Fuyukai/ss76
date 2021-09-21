5b76754cd0a90758f01ebbe0b1cc54e0d5100b9f
Lura Skye Revuwution <l@veriny.tf>
2021-09-21 18:10:05 +0100

    improve car renderer

--- ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/engine/adv/ADVScreen.kt
+++ ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/engine/adv/ADVScreen.kt
@@ -1,6 +1,7 @@
 package tf.veriny.ss76.engine.adv
 
 import com.badlogic.gdx.graphics.OrthographicCamera
+import ktx.app.clearScreen
 import tf.veriny.ss76.SS76
 import tf.veriny.ss76.engine.screen.Screen
 import tf.veriny.ss76.use
@@ -14,6 +15,8 @@ public class ADVScreen(subrenderer: ADVSubRenderer) : Screen {
     public fun isAlreadyRendering(renderer: ADVSubRenderer): Boolean = this.renderer.subRenderer == renderer
 
     override fun render(delta: Float) {
+        clearScreen(0f, 0f, 0f, 1f)
+
         renderer.render(SS76.sceneManager.currentScene)
     }
 }
--- ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/vn/sussex/CarRenderer.kt
+++ ss76-virtual-novel/src/main/kotlin/tf/veriny/ss76/vn/sussex/CarRenderer.kt
@@ -3,10 +3,13 @@ package tf.veriny.ss76.vn.sussex
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
-import tf.veriny.ss76.SS76
+import com.badlogic.gdx.math.Vector
+import com.badlogic.gdx.math.Vector2
 import tf.veriny.ss76.engine.adv.ADVSubRenderer
 import tf.veriny.ss76.engine.scene.SceneState
-import tf.veriny.ss76.use
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
 
 // TODO: Make disposable
 /**
@@ -14,40 +17,80 @@ import tf.veriny.ss76.use
  */
 public class CarRenderer : ADVSubRenderer {
     private companion object {
-        private val ATLAS = TextureAtlas(Gdx.files.internal("gfx/car-renderer.atlas"))
+        private val ATLAS = TextureAtlas(Gdx.files.internal("gfx/ss76.atlas"))
+        private val GRID_SIZE = ATLAS.regions[0].originalWidth
+
+        private val TILES = listOf(
+            "grass", "grass",  // embankment right
+            "road-side", "road-stripe-left",
+            "road-stripe-right", "road-stripe-left",
+            "road-stripe-right", "road-side-2",
+            "grass", "grass-rail-left", "grass-rail-right", "grass",
+            "road-side", "road-stripe-left",
+            "road-stripe-right", "road-stripe-left",
+            "road-stripe-right", "road-side-2",
+            "grass", "grass", "grass"
+        )
     }
 
-    private val tiles = listOf(
-        "grass", "grass", "grass",  // embankment right
-        "road-side", "road-stripe-left",
-        "road-stripe-right", "road-stripe-left", "road-stripe-right", "road-stripe-left",
-        "road-stripe-right", "road-side-2",
-        "grass", "grass-rail-left", "grass-rail-right", "grass",
-        "road-side", "road-stripe-left",
-        "road-stripe-right", "road-stripe-left", "road-stripe-right", "road-stripe-left",
-        "road-stripe-right", "road-side-2",
-        "grass", "grass", "grass"
-    )
+    private class Lane(val gridOffset: Float, val speed: Float) {
+        val cars: MutableList<Vector2> = mutableListOf()
+        var spawnTimer = 0
+    }
 
     private var timer = 0
 
-    private fun renderCars() {
+    private val lanes = arrayOf(
+        Lane(3f, 7f), Lane(7f, 3f)
+    )
+
+    private fun renderCars(batch: SpriteBatch) {
+        // our car (red car) is on the middle lane (overtaking people. what a cunt)
+        val redCar = ATLAS.findRegion("boat-red")
+        val blueCar = ATLAS.findRegion("boat")
+
+        val sineOffset = cos((timer.floorDiv(10).mod(180)).toFloat()) * 2f
+        val redCarPos = (5f * GRID_SIZE) - (redCar.packedWidth / 2) + sineOffset
+        val yOffset = ((16 - 8f) * GRID_SIZE)
+
+        batch.draw(redCar, redCarPos, yOffset)
+
+        for (lane in lanes) {
+            if (lane.cars.size < 3 && lane.spawnTimer <= 0) {
+                var xPos = (lane.gridOffset * GRID_SIZE) - (blueCar.packedWidth / 2)
+                xPos += (sineOffset * 4f)
+                val newPos = Vector2(xPos, 16f * GRID_SIZE)
+                lane.cars.add(newPos)
+                lane.spawnTimer = Random.Default.nextInt(30, 180)
+            }
+
+            for ((idx, car) in lane.cars.toMutableList().withIndex()) {
+                if (car.y < 0f) {
+                    lane.cars.removeAt(idx)
+                } else {
+                    batch.draw(blueCar, car.x, car.y)
+                    car.y -= lane.speed
+                }
+            }
+
+            lane.spawnTimer--
+        }
 
     }
 
     override fun render(batch: SpriteBatch, sceneState: SceneState) {
         var xOffset: Float
-        for (y in 0..16) {
+        for (y in 0..20) {
             xOffset = 0f
-            for (tile in tiles) {
+            for (tile in TILES) {
                 val region = ATLAS.findRegion(tile)!!
                 val offset = timer.mod(region.originalHeight)
-                batch.draw(region, xOffset, y * region.originalHeight.toFloat() - offset)
+                batch.draw(region, xOffset, y * region.originalHeight.toFloat() - (offset * 6))
                 xOffset += region.originalWidth
             }
         }
 
-        renderCars()
+        renderCars(batch)
 
         timer++
     }
